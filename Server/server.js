b9c3a6d4ed020766fd3d1a2a9af29423629af0ce
depinfo <nakasar@outlook.fr>
2017-10-05 11:42:50 +0200

    Pushed WIP on RC for nodejs server.

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,19 @@
-var express = require('express'), app = express(), port = process.env.PORT || 3000;
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 3000,
+mongoose = require('mongoose'),
+user = require('./api/models/giaModel'),
+bodyParser = require('body-parser');
+
+// Init mongoDB instance
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/giadb');
+
+app.use(bodyParser.urlencoded({ 'extended': true}));
+app.use(bodyParser.json())
+
+var routes = require('./api/routes/giaRoutes');
+routes(app);
 
 app.listen(port);
 
