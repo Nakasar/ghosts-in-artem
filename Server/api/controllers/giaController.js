3a27c304f58150fac22e390a23d8b952406bcf91
depinfo <nakasar@outlook.fr>
2017-10-09 11:56:24 +0200

    Added connection node for users.

--- Server/api/controllers/giaController.js
+++ Server/api/controllers/giaController.js
@@ -30,10 +30,10 @@ exports.read_user = function(req, res) {
             res.json(user);
         }
     });
-}
+};
 
 exports.delete_user = function(req, res) {
-    user.remove({
+    User.remove({
         _id: req.params.userId
     }, function(err, user) {
         if (err) {
@@ -42,4 +42,48 @@ exports.delete_user = function(req, res) {
             res.json({ message: 'User successfully deleted' })
         }
     });
+};
+
+exports.user_connect_get = function(req, res) {
+    User.findById(req.params.userId, function(err, user) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(user.connects);
+        }
+    })
+};
+
+exports.user_connect_add = function(req, res) {
+    User.findById(req.params.userId, function(err, user) {
+        if (err) {
+            res.send(err);
+        } else {
+            user.connects.push({ station_id: req.body.station_id, date: Date.now() });
+            user.save(function (err) {
+                if (err) {
+                    res.send(err);
+                }
+
+                res.send( { success : 'Connection taken into account' });
+            });
+        }
+    })
+};
+
+exports.user_connect_whipe = function(req, res) {
+    User.findById(req.params.userId, function(err, user) {
+        if (err) {
+            res.send(err);
+        } else {
+            user.connects = [];
+            user.save(function (err) {
+                if (err) {
+                    res.send(err);
+                }
+
+                res.send( { success : 'User connections whiped.' });
+            });
+        }
+    })
 };
