0c75d380ae06f9391b11304955042ada833bd7e6
LeMilitaire <renaud.felten@depinfonancy.net>
2017-10-23 13:03:39 +0200

    Added phones endpoint.

--- Server/api/controllers/UserController.js
+++ Server/api/controllers/UserController.js
@@ -11,6 +11,16 @@ exports.list_users = function(req, res) {
     });
 };
 
+exports.list_phones = function(req, res) {
+    User.find({}, "bt_mac _id", function(err, users) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(users);
+        };
+    });
+};
+
 exports.create_user = function(req, res) {
     var new_user = new User(req.body);
     new_user.save(function(err, user) {
