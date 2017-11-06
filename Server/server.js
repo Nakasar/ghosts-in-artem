6187b51158195071942c783b0bf2772b9d2ea89f
LeMilitaire <renaud.felten@depinfonancy.net>
2017-11-06 13:32:16 +0100

    Ajout liste deroulante au choix dans l interface

--- Server/server.js
+++ Server/server.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json())
 var routes = require('./api/routes/giaRoutes');
 
 app.all('/*',function(req, res, next){
+  console.log(req);
   res.header("Access-Control-Allow-Origin","*");
   next();
 });
