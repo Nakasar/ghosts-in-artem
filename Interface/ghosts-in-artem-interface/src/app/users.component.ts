43a3ad817f571c0eaeabda624be06671f2819969
THIZY Kevin <Nakasar@users.noreply.github.com>
2017-12-14 10:21:51 +0100

    Fixed crash.

--- Interface/ghosts-in-artem-interface/src/app/users.component.ts
+++ Interface/ghosts-in-artem-interface/src/app/users.component.ts
@@ -14,6 +14,7 @@ import { UserService } from './user.service';
 export class UsersComponent implements OnInit {
   users: User[];
   selectedUser: User;
+  showHide = false;
 
   constructor(
     private router: Router,
